dfa0ac9f7da543dd20deaa1811f6452a499fdcb7
gameless <radiogaga.jin@gmail.com>
2024-07-23 22:23:24 +0900

    upload

---
+++ BlazorApp1/BlazorApp1/Models/CitiesRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazorApp1.Models
+{
+    public static class CitiesRepository
+    {
+        private static List<string> cities = new List<string>()
+        {
+            "Toronto",
+            "Montreal",
+            "Ottawa",
+            "Calgary",
+            "Halifax"
+        };
+
+        public static List<string> GetCities() => cities;
+
+    }
+}
---
+++ BlazorApp1/BlazorApp1/Models/Server.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazorApp1.Models
+{
+    public class Server
+    {
+
+        public int ServerId { get; set; }
+        public bool IsOnline { get; set; }
+        public  string? Name { get; set; }
+        public string? City { get; set; }
+
+        public Server()
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(0, 2);
+            IsOnline = randomNumber == 0 ? false : true;
+        }
+    }
+}
