9c0993da3d6dd856bd07b2074ed6e61d6ff94256
radiogaga <radiogaga@haegin.kr>
2024-08-16 17:58:54 +0900

    update

--- BlazorApp1/BlazorApp1/Models/Server.cs
+++ BlazorApp1/BlazorApp1/Models/Server.cs
@@ -1,11 +1,17 @@
-﻿namespace BlazorApp1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp1.Models
 {
     public class Server
     {
 
         public int ServerId { get; set; }
         public bool IsOnline { get; set; }
+        
+        [Required]
         public  string? Name { get; set; }
+        
+        [Required]
         public string? City { get; set; }
 
         public Server()
--- BlazorApp1/BlazorApp1/Program.cs
+++ BlazorApp1/BlazorApp1/Program.cs
@@ -6,6 +6,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+// builder.Services.AddSingleton<>() // custom abstract 혹은 구현 등
+// builder.Services.AddTransient() // di 컨테이너에서 보관하지 않고 필요할때 인스턴스 만들어서 준다. 
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
